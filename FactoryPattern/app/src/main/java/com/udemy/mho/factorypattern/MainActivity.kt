9b01bdefe29abc60abe5a55ae8dbb6841b609a07
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-19 16:00:55 -0600

    Learning about testing tools.

--- FactoryPattern/app/src/main/java/com/udemy/mho/factorypattern/MainActivity.kt
+++ FactoryPattern/app/src/main/java/com/udemy/mho/factorypattern/MainActivity.kt
@@ -2,9 +2,14 @@ package com.udemy.mho.factorypattern
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,7 @@ class MainActivity : AppCompatActivity() {
                 .append("\n")
                 .append(breadFactory.getBread("SLI")?.name())
                 .toString()
+
+        Log.d(TAG, breadFactory.getBread("SLI")?.name())
     }
 }
