d94bdf0449ee1bc088788716d1daf301a7c6d2ab
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-19 15:35:03 -0600

    Tested BreadFactory.

--- FactoryPattern/app/src/main/java/com/udemy/mho/factorypattern/MainActivity.kt
+++ FactoryPattern/app/src/main/java/com/udemy/mho/factorypattern/MainActivity.kt
@@ -2,11 +2,25 @@ package com.udemy.mho.factorypattern
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createBread()
+    }
+
+    private fun createBread(){
+        val breadFactory = BreadFactory()
+        text_view.text = StringBuilder()
+                .append(breadFactory.getBread("BAG")?.name())
+                .append("\n")
+                .append(breadFactory.getBread("ROL")?.name())
+                .append("\n")
+                .append(breadFactory.getBread("SLI")?.name())
+                .toString()
     }
 }
