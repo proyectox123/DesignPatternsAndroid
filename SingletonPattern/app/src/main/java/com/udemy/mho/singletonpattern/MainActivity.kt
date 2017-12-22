abd0caa42fe1cd70a241207eaffde2a85efef148
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-22 14:34:06 -0600

    Customizing toolbar.

--- SingletonPattern/app/src/main/java/com/udemy/mho/singletonpattern/MainActivity.kt
+++ SingletonPattern/app/src/main/java/com/udemy/mho/singletonpattern/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.udemy.mho.singletonpattern
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import kotlinx.android.synthetic.main.toolbar.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    private fun initToolbar(){
+        toolbar?.let{
+            setSupportActionBar(it)
+            with(it){
+                title = context.getString(R.string.toolbar_title)
+                subtitle = context.getString(R.string.toolbar_subtitle)
+            }
+        }
     }
 }
